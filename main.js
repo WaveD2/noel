1c37eaab183cf73621be6407b5fdc5150503bf09
Wavee64 <tungdev64@gmail.com>
2023-12-20 20:18:59 +0700

    the end

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ const btnLoginSubmit = document.getElementsByClassName("btn-login-submit")[0];
 const boxError = document.getElementsByClassName("box-error")[0];
 const userName = document.getElementById("username");
 const passWord = document.getElementById("password");
-console.log(containerBoxGift, boxCircle);
-console.log(boxMerry, formLogin);
+
 const resultLogin = [
   {
     name: "thanh loan",
@@ -18,7 +17,10 @@ const resultLogin = [
 //check data login
 btnLoginSubmit.addEventListener("click", () => {
   let result = resultLogin.some((item) => {
-    return item.name === userName.value && item.password === passWord.value;
+    return (
+      item.name === userName.value.trim().toLocaleLowerCase() &&
+      item.password === passWord.value.trim()
+    );
   });
   if (!result) {
     boxError.classList.add("d-block");
@@ -38,3 +40,79 @@ btnLoginSubmit.addEventListener("click", () => {
     }, 8000);
   }
 });
+const elts = {
+  text1: document.getElementById("text1"),
+  text2: document.getElementById("text2"),
+};
+
+const texts = ["Em", "là", "bông hoa", "xinh đẹp", "nhất", "của anh"];
+
+const morphTime = 1;
+const cooldownTime = 0.25;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+  morph -= cooldown;
+  cooldown = 0;
+
+  let fraction = morph / morphTime;
+
+  if (fraction > 1) {
+    cooldown = cooldownTime;
+    fraction = 1;
+  }
+
+  setMorph(fraction);
+}
+
+function setMorph(fraction) {
+  elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  fraction = 1 - fraction;
+  elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  elts.text1.textContent = texts[textIndex % texts.length];
+  elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+  morph = 0;
+
+  elts.text2.style.filter = "";
+  elts.text2.style.opacity = "100%";
+
+  elts.text1.style.filter = "";
+  elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  let newTime = new Date();
+  let shouldIncrementIndex = cooldown > 0;
+  let dt = (newTime - time) / 1000;
+  time = newTime;
+
+  cooldown -= dt;
+
+  if (cooldown <= 0) {
+    if (shouldIncrementIndex) {
+      textIndex++;
+    }
+
+    doMorph();
+  } else {
+    doCooldown();
+  }
+}
+
+animate();
